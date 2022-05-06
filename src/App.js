cffa4c481860f48074fef3a4f1e3f5c1b1c50ba5
Monijo <mokajozefowicz@gmail.com>
2022-05-06 18:57:54 +0200

    bootstrap and react-dom configuration.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import{BrowserRouter, Routes, Route} from "react-router-dom"
+import Button from "react-bootstrap/Button"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<div>home</div>}/>
+            <Route path="/book" element={<div><Button variant={"warning"}>abc</Button></div>}/>
+
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
